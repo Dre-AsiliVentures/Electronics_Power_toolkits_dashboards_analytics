1242c423797ac0e1f710419bf2f7d009285b6f46
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-05-25 17:33:21 +0300

    Update electricity_cost_calculator.py

--- electricity_cost_calculator.py
+++ electricity_cost_calculator.py
@@ -1,62 +1,47 @@
-import pandas as pd
 import streamlit as st
 
-# Read the Excel file
-df = pd.read_excel('https://asiliventures.com/wp-content/uploads/2023/05/ElectricityPriceModel.xlsx')
+# Define the appliances and their power ratings
+appliances = {
+    'Television': 150,
+    'Mobile Phone': 10,
+    'Fridge': 200,
+    'Iron Box': 1000,
+    'Electric Kettle': 1500,
+    'Radio Woofer': 80,
+    'Microwave': 1000,
+    'Mixer': 500,
+    'Cooking Appliances': 1500,
+    'Washers': 800,
+    'Dryers': 1500,
+    'Water Dispensers': 120,
+    'Chapati Maker': 1000
+}
 
 # Create Streamlit application
 st.title("Electricity Cost Calculator")
 
 # Checkbox for selecting appliances
-appliances = df['Appliance'].unique()
-selected_appliances = st.multiselect("Select the appliances you own:", appliances)
+selected_appliances = st.multiselect("Select the appliances you own:", list(appliances.keys()))
 
 total_cost = 0
 
 # Iterate over selected appliances
 for appliance in selected_appliances:
-    # Get the row index for the next appliance category
-    next_appliance_index = df[df['Appliance'] == appliance].index.max() + 1
+    # Get power rating for the selected appliance
+    power_rating = appliances[appliance]
 
-    # Get the corresponding data for the selected appliance and previous appliances
-    appliance_data = df.iloc[:next_appliance_index]
+    # Slider for selecting the number of hours the appliance is turned on
+    hours_per_day = st.slider(f"Select the number of hours {appliance} is turned on:", 0, 24, 1)
 
-    # Checkbox for selecting brand
-    brands = appliance_data['Brands'].unique().dropna()
-    selected_brands = st.multiselect(f"Select the brand for {appliance}:", brands)
+    # Input for the rate of electricity in dollars/kWhr
+    rate_per_kwhr = st.number_input("Enter the rate of electricity in dollars/kWhr:", min_value=0.0, step=0.01, value=0.12)
 
-    # Iterate over selected brands
-    for brand in selected_brands:
-        # Get corresponding data for the selected brand
-        brand_data = appliance_data[appliance_data['Brands'] == brand]
+    # Calculate electricity cost for the selected appliance
+    electricity_cost = (power_rating / 1000) * hours_per_day * rate_per_kwhr
+    total_cost += electricity_cost
 
-        # Dropdown menu for selecting model name
-        model_names = brand_data['Model Name'].unique()
-        selected_model_name = st.selectbox(f"Select the model name for {brand} {appliance}:", model_names)
-
-        try:
-            # Filter data for the selected model name
-            model_data = brand_data[brand_data['Model Name'] == selected_model_name]
-
-            if not model_data.empty:
-                # Get the power rating for the selected model name
-                power_rating = model_data['Power Rating (Watts)'].values[0]
-            else:
-                raise IndexError("Selected model name not found.")
-
-            # Calculate electricity cost
-            hours_per_day = st.slider(f"Select the number of hours {brand} {appliance} is turned on:", 0, 24, 1)
-            rate_per_kwhr = st.number_input("Enter the rate of electricity in dollars/kWhr:", min_value=0.0, step=0.01, value=0.12)
-
-            # Calculate electricity cost for the selected appliance
-            electricity_cost = (power_rating / 1000) * hours_per_day * rate_per_kwhr
-            total_cost += electricity_cost
-
-            # Display the calculated electricity cost
-            st.write(f"The electricity cost for {brand} {appliance} ({selected_model_name}) is: ${electricity_cost:.2f}")
-
-        except IndexError:
-            st.write(f"No data found for selected model name: {selected_model_name}. Please try again.")
+    # Display the calculated electricity cost
+    st.write(f"The electricity cost for {appliance} is: ${electricity_cost:.2f}")
 
 # Display the total electricity cost
 st.subheader(f"Total Electricity Cost: ${total_cost:.2f}")
