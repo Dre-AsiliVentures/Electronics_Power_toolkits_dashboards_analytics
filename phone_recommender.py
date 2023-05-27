e8c70b450fd29c9cb91e6c0950aaa6efd45de7fc
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-05-27 17:33:30 +0300

    Update phone_recommender.py

--- phone_recommender.py
+++ phone_recommender.py
@@ -3,9 +3,15 @@ import pandas as pd
 df = pd.read_excel('https://asiliventures.com/wp-content/uploads/2023/05/Phone-Specifications.xlsx')
 def main():
     # Create a checkbox
-    operating_system = st.checkbox("Select Operating System!",list(df['Operating System'].unique()))
-    #operating_system = st.checkbox("Select Operating System!",df['Operating System'].unique())
+    # Sidebar components for user input
+    operating_system = st.selectbox('Select Operating System', list(df['Operating System'].unique()))
 
+    # Filter the data based on the selected operating system
+    filtered_data = df[df['Operating System'] == operating_system]
+
+    # Display the filtered data
+    st.write("Filtered Data:")
+    st.dataframe(filtered_data)
     # Display a message based on the checkbox state
     if operating_system:
         st.write("Checkbox is checked!")
