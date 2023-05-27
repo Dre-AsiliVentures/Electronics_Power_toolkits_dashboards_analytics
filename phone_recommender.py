b90d326ceb3fde24c475befd0d19734e0e59f37d
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-05-27 13:08:09 +0300

    Update phone_recommender.py

--- phone_recommender.py
+++ phone_recommender.py
@@ -1,64 +1,63 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
 
-# Load the phone specifications from the Excel file
-url = 'https://asiliventures.com/wp-content/uploads/2023/05/Phone-Specifications.xlsx'
-df = pd.read_excel(url)
+# Load the phone specifications data from the Excel file
+df = pd.read_excel('https://asiliventures.com/wp-content/uploads/2023/05/Phone-Specifications.xlsx')
 
-# Convert 'Storage' column to numeric type
-df['Storage'] = pd.to_numeric(df['Storage'], errors='coerce')
-
-# Replace NaN values in 'Storage' column with a default value (e.g., 0)
-df['Storage'].fillna(0, inplace=True)
-
-# Remove dollar sign and comma from 'Price' column, and convert it to numeric type
-df['Price'] = df['Price'].str.replace('$', '').str.replace(',', '').astype(float)
-# Create dropdown for operating system
+# Create sidebar components for user input
 operating_system = st.selectbox('Select Operating System', df['Operating System'].unique())
-
-# Create slider for storage space
 storage_space = st.slider('Select Storage Space (GB)', min_value=0, max_value=int(df['Storage'].max()), step=1)
+connectivity_5g = st.checkbox('5G')
+connectivity_4g = st.checkbox('4G')
+connectivity_wifi = st.checkbox('Wi-Fi')
+connectivity_nfc = st.checkbox('NFC')
+design_glass = st.checkbox('Glass')
+design_stainless_steel = st.checkbox('Stainless Steel')
+design_metal = st.checkbox('Metal')
+design_aluminium = st.checkbox('Aluminium')
+design_polycarbonate = st.checkbox('Polycarbonate')
+design_plastic = st.checkbox('Plastic')
+design_gorilla_glass = st.checkbox('Gorilla Glass')
+security_face_id = st.checkbox('Face ID or Facial Recognition')
+security_rear_mounted = st.checkbox('Rear-Mounted')
+security_in_display = st.checkbox('In-Display')
+stylus_pen_support = st.checkbox('Stylus Pen Support')
+magsafe_compatibility = st.checkbox('Magsafe Compatibility')
+display_amoled_oled = st.slider('Select Display (AMOLED & OLED)', min_value=0, max_value=df['Display (AMOLED & OLED)'].max(), step=1)
+pop_up_camera = st.checkbox('Pop-up Camera')
+vr_support = st.checkbox('VR Support')
+optical_image_stabilization = st.checkbox('Optical Image Stabilization')
+price_range = st.slider('Select Price Range', min_value=0, max_value=int(df['Price'].str.replace('$', '').astype(int).max()), step=100)
 
-# Create checkboxes for connectivity
-connectivity_options = ['5G', '4G', 'Wi-Fi', 'NFC']
-connectivity_selected = st.multiselect('Select Connectivity', connectivity_options, connectivity_options)
-
-# Create checkboxes for design and build quality
-design_options = ['Glass', 'Stainless Steel', 'Metal', 'Aluminium', 'Polycarbonate', 'Plastic', 'Gorilla Glass']
-design_selected = st.multiselect('Select Design & Build Quality', design_options, design_options)
-
-# Create checkboxes for security and privacy
-security_options = ['Face ID', 'Facial Recognition', 'Rear-Mounted', 'In-Display']
-security_selected = st.multiselect('Select Security & Privacy Features', security_options, security_options)
-
-# Create slider for IP rating
-ip_rating = st.slider('Select IP Rating (IP67 or IP68)', min_value=67, max_value=68, step=1)
-
-# Create checkboxes for additional features
-additional_features = ['Stylus Pen Support', 'Magsafe Compatibility', 'AMOLED & OLED Display', 'Pop-up Camera', 'VR Support', 'Optical Image Stabilization']
-additional_selected = st.multiselect('Select Additional Features', additional_features, additional_features)
-
-# Create slider for USB type
-usb_type = st.slider('Select USB Type (USB Type-C or Type-B)', min_value=0, max_value=1, step=1)
-
-# Create slider for price range
-price_range = st.slider('Select Price Range', min_value=0, max_value=int(df['Price'].max()), step=100)
-
-# Filter the dataframe based on the selected criteria
+# Filter the DataFrame based on the selected criteria
 filtered_df = df[
     (df['Operating System'] == operating_system) &
     (df['Storage'] >= storage_space) &
-    (df['Connectivity'].apply(lambda x: all(option in x for option in connectivity_selected))) &
-    (df['Design & Build Quality'].apply(lambda x: all(option in x for option in design_selected))) &
-    (df['Security & Privacy'].apply(lambda x: all(option in x for option in security_selected))) &
-    (df['Additional Features'].apply(lambda x: all(option in x for option in additional_selected))) &
-    (df['USB Type (Type C)'] == usb_type_c) &
-    (df['USB Type (Type B)'] == usb_type_b) &
-    (df['Price'] <= price_range)
+    (df['Connectivity (5G)'] == connectivity_5g) &
+    (df['Connectivity (4G)'] == connectivity_4g) &
+    (df['Connectivity (Wi-Fi)'] == connectivity_wifi) &
+    (df['Connectivity (NFC)'] == connectivity_nfc) &
+    (df['Design & Build Quality (Glass)'] == design_glass) &
+    (df['Design & Build Quality (Stainless Steel)'] == design_stainless_steel) &
+    (df['Design & Build Quality (Metal)'] == design_metal) &
+    (df['Design & Build Quality (Aluminium)'] == design_aluminium) &
+    (df['Design & Build Quality (Polycarbonate)'] == design_polycarbonate) &
+    (df['Design & Build Quality (Plastic)'] == design_plastic) &
+    (df['Design & Build Quality (Gorilla Glass)'] == design_gorilla_glass) &
+    (df['Security & Privacy (Face ID or Facial Recognition)'] == security_face_id) &
+    (df['Security & Privacy (Rear-Mounted)'] == security_rear_mounted) &
+    (df['Security & Privacy (In-Display)'] == security_in_display) &
+    (df['Stylus Pen Support'] == stylus_pen_support) &
+    (df['Magsafe Compatibility'] == magsafe_compatibility) &
+    (df['Display (AMOLED & OLED)'] >= display_amoled_oled) &
+    (df['Pop-up Camera'] == pop_up_camera) &
+    (df['VR Support'] == vr_support) &
+    (df['Optical Image Stabilization'] == optical_image_stabilization) &
+    (df['Price'].str.replace('$', '').astype(int) >= price_range)
 ]
 
-# Display the filtered phones
+# Display the filtered results
 if len(filtered_df) > 0:
-    st.table(filtered_df)
+    st.write(filtered_df)
 else:
-    st.warning('No phones match the selected criteria.')
+    st.write('No phones match the selected criteria.')
