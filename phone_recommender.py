f87fa0f5f70092e7591c862c56d894c53e743d33
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-05-27 17:45:05 +0300

    Update phone_recommender.py

--- phone_recommender.py
+++ phone_recommender.py
@@ -11,14 +11,44 @@ def main():
     with st.beta_expander('Connectivity Options'):
         with st.beta_container():
             col1, col2, col3, col4 = st.beta_columns(4)
-        with col1:
-            is_5g = st.checkbox('5G')
-        with col2:
-            is_4g = st.checkbox('4G')
-        with col3:
-            is_wifi = st.checkbox('Wi-Fi')
-        with col4:
-            is_nfc = st.checkbox('NFC')
+            with col1:
+                is_5g = st.checkbox('5G')
+            with col2:
+                is_4g = st.checkbox('4G')
+            with col3:
+                is_wifi = st.checkbox('Wi-Fi')
+            with col4:
+                is_nfc = st.checkbox('NFC')
+
+    st.subheader('Design & Build Quality')
+    with st.beta_expander('Design & Build Quality Options'):
+        with st.beta_container():
+            col1, col2, col3, col4, col5, col6, col7 = st.beta_columns(7)
+            with col1:
+                is_glass = st.checkbox('Glass')
+            with col2:
+                is_stainless_steel = st.checkbox('Stainless Steel')
+            with col3:
+                is_metal = st.checkbox('Metal')
+            with col4:
+                is_aluminium = st.checkbox('Aluminium')
+            with col5:
+                is_polycarbonate = st.checkbox('Polycarbonate')
+            with col6:
+                is_plastic = st.checkbox('Plastic')
+            with col7:
+                is_gorilla_glass = st.checkbox('Gorilla Glass')
+
+    st.subheader('Security & Privacy')
+    with st.beta_expander('Security & Privacy Options'):
+        with st.beta_container():
+            col1, col2, col3 = st.beta_columns(3)
+            with col1:
+                is_face_id = st.checkbox('Face ID or Facial Recognition')
+            with col2:
+                is_rear_mounted = st.checkbox('Rear-Mounted')
+            with col3:
+                is_in_display = st.checkbox('In-Display')
     # Filter the data based on the selected operating system
     #filtered_data = df[df['Operating System'] == operating_system]
     st.write(operating_system)
