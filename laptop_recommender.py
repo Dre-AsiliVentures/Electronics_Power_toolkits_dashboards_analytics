ca3d61f3cc5982ff4777a8cadd22c9548968bdfc
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-06-01 13:19:01 +0300

    Update laptop_recommender.py

--- laptop_recommender.py
+++ laptop_recommender.py
@@ -3,7 +3,6 @@ import pandas as pd
 
 # Load the laptop specifications table
 df = pd.read_excel('https://asiliventures.com/wp-content/uploads/2023/06/Laptop-Specifications.xlsx')
-
 # Create a sidebar
 st.sidebar.markdown('**Laptop Recommendations**')
 
@@ -27,12 +26,13 @@ st.markdown('**Laptop Recommendations**')
 
 # If the user has entered any requirements, filter the table and display the results
 if intel_core_version != 1 or fingerprint_face_id or ram != 8 or internal_storage != 512 or price != 1000:
-    filtered_df = df[(df['Intel Core Version'] == intel_core_version) & (df['Fingerprint/Face ID'].isin([True, False])) & (df['RAM'] >= ram) & (df['Internal Storage'] >= internal_storage) & (df['Price'] <= price)]
+    filtered_df = df[(df['Intel Core Version'] == intel_core_version) & (df['Fingerprint/Face ID'] == 'Yes') & (df['RAM'] >= ram) & (df['Internal Storage'] >= internal_storage) & (df['Price'] <= price)]
     st.table(filtered_df)
 
 # Otherwise, display all the laptops in the table
 else:
     st.table(df)
+
 
 
 
