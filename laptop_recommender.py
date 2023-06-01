dd6d7fb22c9763524a11af1632c06cc867e1909a
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-06-01 13:12:16 +0300

    Update laptop_recommender.py

--- laptop_recommender.py
+++ laptop_recommender.py
@@ -9,16 +9,6 @@ st.sidebar.markdown('**Laptop Recommendations**')
 # Create a slider for the Intel Core version
 intel_core_version = st.sidebar.slider('Intel Core Version', min_value=1, max_value=12, value=1)
 
-# Create a checkbox for touchscreen compatibility
-touchscreen = st.sidebar.checkbox('Touchscreen')
-
-# Create a dropdown menu for the display type
-display_type = st.sidebar.selectbox('Display Type',
-    options=['IPS', 'OLED', 'QHD', '4K'])
-
-# Create a slider for the display resolution
-display_resolution = st.sidebar.slider('Display Resolution', min_value=1080, max_value=4096, value=1920)
-
 # Create a checkbox for fingerprint/face ID availability
 fingerprint_face_id = st.sidebar.checkbox('Fingerprint/Face ID')
 
@@ -35,11 +25,18 @@ price = st.sidebar.slider('Price', min_value=500, max_value=5000, value=1000)
 st.markdown('**Laptop Recommendations**')
 
 # If the user has entered any requirements, filter the table and display the results
-if intel_core_version != 1 or touchscreen or display_type or display_resolution != 1920 or fingerprint_face_id or ram != 8 or internal_storage != 512 or price != 1000:
-    filtered_df = df[(df['Intel Core Version'] == intel_core_version) & (df['Touchscreen'] == touchscreen) & (df['Display Type'] == display_type) & (df['Display Resolution'] == display_resolution) & (df['Fingerprint/Face ID'] == fingerprint_face_id) & (df['RAM'] >= ram) & (df['Internal Storage'] >= internal_storage) & (df['Price'] <= price)]
+if intel_core_version != 1 or fingerprint_face_id or ram != 8 or internal_storage != 512 or price != 1000:
+    filtered_df = df[(df['Intel Core Version'] == intel_core_version) & (df['Fingerprint/Face ID'] == fingerprint_face_id) & (df['RAM'] >= ram) & (df['Internal Storage'] >= internal_storage) & (df['Price'] <= price)]
+
+    # Check if the user wants a touchscreen laptop
+    touchscreen = st.sidebar.checkbox('Touchscreen')
+    if touchscreen:
+        filtered_df = filtered_df[filtered_df['Touchscreen'] == 'Yes']
+
     st.table(filtered_df)
 
 # Otherwise, display all the laptops in the table
 else:
     st.table(df)
+
 
