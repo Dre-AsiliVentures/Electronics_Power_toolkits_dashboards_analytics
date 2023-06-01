1ae855636150a151e8063513f73659ade81f81e2
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-06-01 12:40:16 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+# Create a title
+st.title('Streamlit App with st.expander')
+
+# Create a sidebar
+st.sidebar.markdown('This is the sidebar.')
+
+# Create a main area
+st.main.markdown('This is the main area.')
+
+# Create an expander
+with st.expander('Expander'):
+  st.markdown('This is the content of the expander.')
