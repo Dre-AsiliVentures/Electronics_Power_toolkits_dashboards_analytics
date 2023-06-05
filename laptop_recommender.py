cda658574c8749afa6350320f29bb49fc0e392b1
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-06-05 14:21:57 +0300

    Update laptop_recommender.py

--- laptop_recommender.py
+++ laptop_recommender.py
@@ -4,43 +4,52 @@ import pandas as pd
 # Load the laptop specifications table
 df = pd.read_excel('https://asiliventures.com/wp-content/uploads/2023/06/Laptop-Specifications.xlsx')
 
-# Load the laptop specifications table
-df = pd.read_excel('https://asiliventures.com/wp-content/uploads/2023/06/Laptop-Specifications.xlsx')
+def convert_storage_capacity(value):
+    if value.endswith('GB'):
+        return int(value.split('GB')[0])
+    elif value.endswith('TB'):
+        return int(value.split('TB')[0]) * 1024
+    else:
+        return 0
 
-# Create a sidebar
-st.sidebar.markdown('**Laptop Recommendations**')
+def main():
+    intel_core_version = st.selectbox('Select Intel Core Version', list(df['Intel Core Version'].unique()))
+    with st.expander('Capacity & Storage'):
+        internal_storage = st.slider('Internal Storage (in GB)', min_value=32, max_value=2048, value=512)
+        hdd_or_ssd = st.selectbox('HDD or SSD or eMMC', list(df['HDD/SSD Classification'].unique()))
+        ram = st.slider('RAM (in GB)', min_value=4, max_value=64, value=8)
+    # Create a checkbox for Backlight Keyboard
+    if st.checkbox('Backlight Keyboard'):
+        df = df[df['Backlight Keyboard Availability'] == 'Yes']
+    if st.checkbox('Touchscreen Compatibility'):
+        df = df[df['Touchscreen Compatibility'] == 'Yes']
+    generation_version = st.selectbox('Select Generation Version', list(df['Generation Version'].unique()))
+    with st.expander('Display Features'):
+        display_type = st.selectbox('Display Type', list(df['Display Type'].unique()))
+        display_quality = st.selectbox('Display Quality', list(df['Display Quality'].unique()))
+    if st.checkbox('Sim Card Slot Availability'):
+        df = df[df['Sim Card Slot Availability'] == 'Yes']
+    if st.checkbox('Fingerprint/Face ID Availability'):
+        df = df[df['Fingerprint/Face ID Availability'] == 'Yes']
+    if st.checkbox('Bluetooth Availability'):
+        df = df[df['Bluetooth Availability'] == 'Yes']
+    no_of_usb = st.selectbox('No. of USB Ports', list(df['No. of USB Ports'].unique()))
+    if st.checkbox('Filter by Weight'):
+        weight_unit = st.radio("Select weight unit:", ('Pounds', 'Kgs'))
+        if weight_unit == 'Pounds':
+            weight = st.selectbox('Laptop Weight in Pounds', list(df['Weight in Pounds'].unique()))
+            df = df[df['Weight in Pounds'] == weight]
+        else:
+            weight = st.selectbox('Laptop Weight in Kgs', list(df['Weight in Kgs'].unique()))
+            df = df[df['Weight in Kgs'] == weight]
+    price = st.slider('Price', min_value=500, max_value=5000, value=300)
 
-# Create a slider for the Intel Core version
-intel_core_version = st.sidebar.slider('Intel Core Version', min_value=1, max_value=12, value=1)
+    if st.button('Recommend Laptops'):
+        recommended_df = df[(df['Intel Core Version'] == intel_core_version) &
+                            (df['RAM Storage Capacity'].apply(convert_storage_capacity) >= ram) &
+                            (df['Internal Storage Capacity'].apply(convert_storage_capacity) >= internal_storage) &
+                            (df['Price'] <= price)]
+        st.table(recommended_df[['Laptop Model Name']])
 
-# Create a checkbox for fingerprint/face ID
-fingerprint_face_id = st.sidebar.checkbox('Fingerprint/Face ID')
-
-# Create a slider for the RAM
-ram = st.sidebar.slider('RAM', min_value=4, max_value=64, value=8)
-
-# Create a slider for the internal storage
-internal_storage = st.sidebar.slider('Internal Storage', min_value=32, max_value=2048, value=512)
-
-# Convert the internal storage slider value to GB
-internal_storage_gb = internal_storage
-
-# Create a slider for the price
-price = st.sidebar.slider('Price', min_value=500, max_value=5000, value=1000)
-
-# Create a main area
-st.markdown('**Laptop Recommendations**')
-
-# If the user has entered any requirements, filter the table and display the results
-if intel_core_version != 1 or fingerprint_face_id or ram != 8 or internal_storage_gb != 512 or price != 1000:
-    filtered_df = df[(df['Intel Core Version'] == intel_core_version) & (df['Fingerprint/Face ID'].isin(['Yes'])) & (df['RAM'] >= int(ram.split('GB')[0])) & (df['Internal Storage Capacity'] >= int(internal_storage_gb.split('GB')[0])) & (df['Price'] <= price)]
-    st.table(filtered_df)
-
-# Otherwise, display all the laptops in the table
-else:
-    st.table(df)
-
-
-
-
-
+if __name__ == "__main__":
+    main()
